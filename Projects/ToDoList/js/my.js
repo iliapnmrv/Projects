16b1ed45b2eac8d3eec2ae2bb09376e4f75476c1
Ilya <ilyapnmrv>
2020-04-28 19:28:47 +0300

    Обновления

--- Projects/ToDoList/js/my.js
+++ Projects/ToDoList/js/my.js
@@ -1,5 +1,18 @@
 //var block =;
-//$('textarea').autoResize();
+var textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keydown', autosize);
+             
+function autosize(){
+  var el = this;
+  setTimeout(function(){
+    el.style.cssText = 'height:auto; padding:0';
+    // for box-sizing other than "content-box" use:
+    // el.style.cssText = '-moz-box-sizing:content-box';
+    el.style.cssText = 'height:' + el.scrollHeight + 'px';
+  },0);
+}
+
 function add() {
     block.insertAdjacentHTML('beforeend', '<div class="block" id="one"><div class="title">Я блок</div><textarea placeholder="Создайте заметку нажмите Alt + N для доступа к полю заполнения" accesskey="n" name="" id=""></textarea></div>');
 }
