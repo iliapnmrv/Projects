5c57aa7b4d13a58c1610f3b8c15722f93eb95f67
Ilya <ilyapnmrv>
2020-04-28 22:44:45 +0300

    Обновления

--- Projects/ToDoList/js/my.js
+++ Projects/ToDoList/js/my.js
@@ -7,14 +7,12 @@ function autosize(){
   var el = this;
   setTimeout(function(){
     el.style.cssText = 'height:auto; padding:0';
-    // for box-sizing other than "content-box" use:
-    // el.style.cssText = '-moz-box-sizing:content-box';
     el.style.cssText = 'height:' + el.scrollHeight + 'px';
   },0);
 }
 
 function add() {
-    block.insertAdjacentHTML('beforeend', '<div class="block" id="one"><div class="title">Я блок</div><textarea placeholder="Создайте заметку нажмите Alt + N для доступа к полю заполнения" accesskey="n" name="" id=""></textarea></div>');
+    block.insertAdjacentHTML('beforeend', '<label class="container"><input type="checkbox"><span class="checkmark"></span></label><div class="block" id="one"><div class="title">Я блок</div><textarea placeholder="Создайте заметку нажмите Alt + N для доступа к полю заполнения" accesskey="n" name="" id=""></textarea></div>');
 }
 function remove() {
     document.getElementById("one").remove();
@@ -24,3 +22,20 @@ function checkbox() {
     var checkbox = document.querySelector('checkbox');
     checkbox.classList.add("green");
 }
+
+var mywindow = $(window);
+var mypos = mywindow.scrollTop();
+var up = false;
+var newscroll;
+mywindow.scroll(function () {
+    newscroll = mywindow.scrollTop();
+    if (newscroll > mypos && !up) {
+        $('.banner').stop().fadeOut();
+        up = !up;
+        console.log(up);
+    } else if(newscroll < mypos && up) {
+        $('.banner').stop().fadeIn();
+        up = !up;
+    }
+    mypos = newscroll;
+});
