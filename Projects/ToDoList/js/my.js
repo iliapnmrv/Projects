f5057e490951aa8a602272dba5a5922f35844af0
Ilya <ilyapnmrv>
2020-05-21 01:25:40 +0300

    Обновления

--- Projects/ToDoList/js/my.js
+++ Projects/ToDoList/js/my.js
@@ -1,8 +1,7 @@
 //var block =;
-var textarea = document.querySelector('textarea');
-
+let textarea = document.querySelector('.textarea');
 textarea.addEventListener('keydown', autosize);
-             
+
 function autosize(){
   var el = this;
   setTimeout(function(){
@@ -12,8 +11,11 @@ function autosize(){
 }
 
 function add() {
-    block.insertAdjacentHTML('beforeend', '<label class="container"><input type="checkbox"><span class="checkmark"></span></label><div class="block" id="one"><div class="title">Я блок</div><textarea placeholder="Создайте заметку нажмите Alt + N для доступа к полю заполнения" accesskey="n" name="" id=""></textarea></div>');
+    let code = "<li><div class='block'><div class='title'>Я блок</div><textarea class='textarea' placeholder='Создайте заметку нажмите Alt + N для доступа к полю заполнения' accesskey='n'></textarea></div></li>";
+    let ol = document.querySelector('ol');
+    ol.insertAdjacentHTML('beforeend', code);
 }
+
 function remove() {
     document.getElementById("one").remove();
 }
@@ -22,7 +24,13 @@ function checkbox() {
     var checkbox = document.querySelector('checkbox');
     checkbox.classList.add("green");
 }
+//drag and drop
+new Sortable(list, {
+    animation: 150,
+    ghostClass: 'blue-background-class'
+});
 
+//меню наверху
 var mywindow = $(window);
 var mypos = mywindow.scrollTop();
 var up = false;
@@ -39,3 +47,21 @@ mywindow.scroll(function () {
     }
     mypos = newscroll;
 });
+
+//  UNRELEASED YET
+
+//new Sortable(example3Left, {
+//    group: {
+//        name: 'shared',
+//        pull: 'clone' // To clone: set pull to 'clone'
+//    },
+//    animation: 150
+//});
+//
+//new Sortable(example3Right, {
+//    group: {
+//        name: 'shared',
+//        pull: 'clone'
+//    },
+//    animation: 150
+//});
